6bb5fc31e412a625102250142d0eb0d4599a72e7
ankanbanerjee12345@gmail.com <ankanbanerjee12345@gmail.com>
2025-07-10 04:07:12 +0530

    create home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 
 import './App.css'
+import Home from "./pages/Home";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <Home />
   )
 }
 
