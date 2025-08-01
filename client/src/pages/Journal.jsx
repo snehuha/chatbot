e8d20461095fd1cb2b782c2e1f3b017213c206a0
snehuha <sneha310705@gmail.com>
2025-08-01 20:59:37 +0530

    journal page edit

--- client/src/pages/Journal.jsx
+++ client/src/pages/Journal.jsx
@@ -10,20 +10,21 @@ export default function Journal() {
     <div className="min-h-screen bg-sky-200 p-6 flex flex-col items-center">
       <Header />
       <div className="text-center mb-8">
-        <button className="bg white px-4 py=4 inline-block text-3xl font-serif border-2 border-black rounded-lg px-6 py-2 shadow-md bg-gray-100 mb-4">
-          Your Journal
+        <button className="bg white px-4 py-4 h-20 w-20 inline-block text-7xl font-serif border-20 border-black rounded-lg px-6 py-2 shadow-md bg-gray-100 mb-4">
+         <h1>Your Journal</h1> 
         </button>
         
         <p className="text-gray-700 italic">Let it out, one word at a time.</p>
       </div>
 
-      <div className="w-full max-w-3xl">
-        <div className="flex gap- mb-8">
-          <button className="bg-white px-4 py-4 rounded border-4 border-black shadow hover:bg-gray-100 transition-colors ">
-            Prompt
+      <div className="w-full max-w-3xl border-2 p-10">
+      <div className="space-x-2 flex gap-8 mb-10 ml-20 ">
+
+          <button className="bg-white px-4 py-4 rounded border-4 border-black-100 shadow hover:bg-gray-100 transition-colors mb-10">
+           <h4>Prompt</h4> 
           </button>
-          <button className="bg-white px-4 py-2 rounded border shadow hover:bg-gray-100 transition-colors">
-            To Save
+          <button className="bg-white px-4 py-2 rounded border-[5px] shadow hover:bg-gray-100 transition-colors">
+           <h4>To Save</h4> 
           </button>
         </div>
     
