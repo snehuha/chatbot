0ccfa971d8be6301df4ab5c7154b46a5b26eb911
snehuha <sneha310705@gmail.com>
2025-10-12 23:44:43 +0530

    pause page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const Home = lazy(() => import("./pages/Home"));
 const Journal = lazy(() => import("./pages/Journal"));
 const Talk = lazy(() => import("./pages/Talk"));
+const Pause = lazy(() => import("./pages/Pause"));
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/talk" element={<Talk />} />
           <Route path="/journal" element={<Journal />} />
+          <Route path="/pause" element={<Pause/>}/>
         </Routes>
       </Suspense>
     </Router>
